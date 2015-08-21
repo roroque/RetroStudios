a4ee45fec5c65b3bc823a89c594d5d41e4e367f3
Igor Avila Amaral de Souza <igoravila93@gmail.com>
2015-08-21 15:55:06 -0300

    Auxiliary classes to create the SK nodes

---
+++ Power Pong/Power Pong/NodesCreator.swift
@@ -0,0 +1,99 @@
+//
+//  LabelsCreator.swift
+//  Power Pong
+//
+//  Created by Igor Avila Amaral de Souza on 8/21/15.
+//  Copyright (c) 2015 Retro Studios. All rights reserved.
+//
+
+import SpriteKit
+
+
+////Score Labels
+//self.playerOneScoreNode = SKLabelNode(fontNamed: "Helvetica")
+//
+//self.playerOneScoreNode.fontColor = SKColor.whiteColor()
+//self.playerOneScoreNode.fontSize = scoreFontSize
+//self.playerOneScoreNode.position = CGPointMake(size.width * 0.25, size.height - scoreFontSize * 2.0)
+//self.playerTwoScoreNode.position = CGPointMake(size.width * 0.75, size.height - scoreFontSize * 2.0)
+//
+//self.addChild(self.playerOneScoreNode)
+//self.addChild(self.playerTwoScoreNode)
+//
+////Restart node
+//self.restartGameNode = SKSpriteNode(imageNamed: "restartNode.png")
+//self.restartGameNode.size = CGSizeMake(restartNodeWidthHeight, restartNodeWidthHeight)
+//self.restartGameNode.position = CGPointMake(size.width / 2.0, size.height - restartNodeWidthHeight)
+//self.restartGameNode.hidden = true
+//self.addChild(self.restartGameNode)
+//
+////start game info node
+//self.startGameInfoNode = SKLabelNode(fontNamed: "Helvetica")
+//self.startGameInfoNode.fontColor = SKColor.whiteColor()
+//self.startGameInfoNode.fontSize = scoreFontSize
+//self.startGameInfoNode.position = CGPointMake(size.width / 2.0, size.height / 2.0)
+//self.startGameInfoNode.text = "Tap to start!"
+//self.addChild(self.startGameInfoNode)
+
+
+class NodesCreator: NSObject {
+    
+    static func createScoreLabel(font: String, fontSize: CGFloat, color: SKColor, xPos: CGFloat, yPos: CGFloat) -> SKLabelNode {
+        var label = SKLabelNode(fontNamed: font)
+        label.fontColor = color
+        label.fontSize = fontSize
+        label.position = CGPointMake(xPos, yPos)
+        return label
+    }
+    
+ 
+    static func createInfoLabel(font: String, fontSize: CGFloat, color: SKColor, xPos: CGFloat, yPos: CGFloat, text: String) -> SKLabelNode{
+        var label = SKLabelNode(fontNamed: "Helvetica")
+        label.fontColor = color
+        label.fontSize = fontSize
+        label.position = CGPointMake(xPos, yPos)
+        label.text = text
+        
+        return label
+    }
+    
+    
+    static func createRestartGameNode(image: String, height: CGFloat, width: CGFloat, xPos: CGFloat, yPos: CGFloat) -> SKSpriteNode{
+        var restartGameNode = SKSpriteNode(imageNamed: image)
+        restartGameNode.size = CGSizeMake(height, width)
+        restartGameNode.position = CGPointMake(xPos, yPos)
+        restartGameNode.hidden = true
+        
+        return restartGameNode
+        
+    }
+    
+    static func createBall(ballWidth: CGFloat, ballHeight: CGFloat, ballRadius: CGFloat, category: UInt32, contact: UInt32, xPos: CGFloat, yPos: CGFloat) -> SKSpriteNode {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        var ballNode : SKSpriteNode
+        if let name = defaults.stringForKey("Ball"){
+            ballNode = SKSpriteNode(imageNamed: "\(name).png")
+        }else{
+            ballNode = SKSpriteNode(imageNamed: "circleNode.png")
+        }
+        ballNode.size = CGSizeMake(ballWidth, ballHeight)
+        ballNode.physicsBody = SKPhysicsBody(circleOfRadius: ballRadius)
+        ballNode.physicsBody!.categoryBitMask = category
+        ballNode.physicsBody!.contactTestBitMask = contact
+        ballNode.physicsBody!.linearDamping = 0.0
+        ballNode.physicsBody!.angularDamping = 0.0
+        ballNode.physicsBody!.restitution = 1.0
+        ballNode.physicsBody!.dynamic = true
+        ballNode.physicsBody!.friction = 0.0
+        ballNode.physicsBody!.allowsRotation = false
+        ballNode.position = CGPointMake(xPos, yPos)
+        
+        return ballNode
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init()
+    }
+}
+
